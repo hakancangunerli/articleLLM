b50f6ac6b4a1aa89459af0fc9241b517ad7253d3
John Gunerli <8197139@ups.com>
2023-07-12 18:02:18 -0400

    updated working copy

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from chat import chatsection
+from streamlit_chat import message
 
-st.title("UPS Chat")
+st.title("ChatBot Interface")
 
+import os
+os.environ['CURL_CA_BUNDLE'] = '' # per https://stackoverflow.com/a/75746105
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -10,15 +13,20 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
+with st.chat_message("assistant"):
+    st.write("Hi, How can I assist you?")
+    
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        message_placeholder = st.write("Hi")
-        full_response = ""
-        full_response += chatsection(prompt).delta.get("content", "")
-        message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+        with st.spinner("Retrieving the data for you, please wait"):
+            message_placeholder = st.empty()
+            full_response = ""
+            result_from_chat = chatsection(prompt)
+            print(prompt, result_from_chat['answer'])
+            full_response += chatsection(prompt)['answer']
+            message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
--- chat.py
+++ chat.py
@@ -9,31 +9,27 @@ from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddi
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.document_loaders import TextLoader
-def chatsection(prompt):
-#If we have a pdf, we can convert pdf to text
-# # %pip install PyPDF2
-# from PyPDF2 import PdfReader
-# reader = PdfReader("./swiss_constitution.pdf")
-# number_of_pages = len(reader.pages)
-# for i in range(number_of_pages):
-#     page = reader.pages[i]
-#     text = page.extract_text()
-#     with open("./swiss_constitution_text.txt", "a") as f:
-#         f.write(text)
 
+import os
+os.environ['CURL_CA_BUNDLE'] = '' # per https://stackoverflow.com/a/75746105
+
+
+def chatsection(prompt):
     raw_documents = TextLoader('test.txt',encoding='utf8').load()
     text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
     documents = text_splitter.split_documents(raw_documents)
 
+    print("calling sentence transformer")
     embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
+    print("calling database search")
     db = Chroma.from_documents(documents, embedding_function, persist_directory="./vectordb")
 
     query = prompt
     #"What was the US' economy like in 1850?"
-    print("Conducting Sim Search")
+    print("Conducting similarity search")
     docs = db.similarity_search(query)
-    print(docs[0].page_content)
+    # print(docs[0].page_content)
 
     model_name = "deepset/roberta-base-squad2"
 
@@ -49,5 +45,5 @@ def chatsection(prompt):
     model = AutoModelForQuestionAnswering.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # print(res)
+    print(res)
     return res
